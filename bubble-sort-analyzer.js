5baad160ec076a6781c5a3fed609eacc7d69f352
sulubaby <pokefanshere@gmail.com>
2026-04-20 21:06:22 +0300

    Update bubble-sort-analyzer.js

--- bubble-sort-analyzer.js
+++ bubble-sort-analyzer.js
@@ -1,25 +1,14 @@
 function bubbleSortAnalyzer(a, comparator) {
   let iterations = 0;
   let swaps = 0;
-
   for (let i = 0; i < a.length - 1; i++) {
     for (let j = 0; j < a.length - 1 - i; j++) {
       iterations++;
-
-      let left = a[j];
-      let right = a[j + 1];
-
-      if (comparator(left, right) > 0) {
-        a[j] = right;
-        a[j + 1] = left;
+      if (comparator(a[j], a[j + 1]) > 0) {
+        [a[j], a[j + 1]] = [a[j + 1], a[j]];
         swaps++;
       }
     }
   }
-
-  return {
-    sortedArray: a,
-    iterations: iterations,
-    swaps: swaps
-  };
+  return { sortedArray: a, iterations, swaps };
 }
